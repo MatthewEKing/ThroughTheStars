a988f21f7ae770092e7278b97878a2be4307a69c
Matt <matt.king1119@gmail.com>
2022-02-21 11:43:47 -0500

    Add files via upload

---
+++ Scripts/CollisionHandler.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    [SerializeField] AudioClip successSFX;
+    [SerializeField] AudioClip crashSFX;
+
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem crashParticles;
+
+    [SerializeField] float levelLoadDelay = 1f;
+
+    bool isTransitioning = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        if (isTransitioning)
+        {
+            return;
+        }
+
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Collided w/ Friendly");
+                break;
+
+            case "Finish":
+                StartSucessSequence();
+                break;
+
+            default:
+                StartCrashSequence();
+                break;
+        }
+    }
+
+
+    void StartSucessSequence()
+    {
+        isTransitioning = true;
+        audioSource.Stop();
+        successParticles.Play();
+        GetComponent<Movement>().enabled = false;
+        audioSource.PlayOneShot(successSFX);
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    void StartCrashSequence()
+    {
+        isTransitioning = true;
+        audioSource.Stop();
+        crashParticles.Play();
+        GetComponent<Movement>().enabled = false;
+        audioSource.PlayOneShot(crashSFX);
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
--- Scripts/Movement.cs
+++ Scripts/Movement.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     Rigidbody rb;
+    AudioSource audioSource;
 
+    [SerializeField] AudioClip thrusterSFX;
+    [SerializeField] ParticleSystem thrusterParticles;
+    [SerializeField] ParticleSystem rightThrusterParticles;
+    [SerializeField] ParticleSystem leftThrusterParticles;
 
     [SerializeField] float thrustSpeed = 1000f;
     [SerializeField] float rotateSpeed = 1000f;
@@ -13,6 +18,7 @@ public class Movement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -26,6 +32,20 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * thrustSpeed * Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(thrusterSFX);
+            }
+
+            if (!thrusterParticles.isPlaying)
+            {
+                thrusterParticles.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+            thrusterParticles.Stop();
         }
     }
 
@@ -34,14 +54,31 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             ApplyRotation(rotateSpeed);
+
+            if (!leftThrusterParticles.isPlaying)
+            {
+                leftThrusterParticles.Play();
+            }
+
         }
         else if (Input.GetKey(KeyCode.D))
         {
             ApplyRotation(-rotateSpeed);
+
+
+            if (!rightThrusterParticles.isPlaying)
+            {
+                rightThrusterParticles.Play();
+            }
+        }
+        else
+        {
+            leftThrusterParticles.Stop();
+            rightThrusterParticles.Stop();
         }
     }
 
-    private void ApplyRotation(float rotationDir)
+    void ApplyRotation(float rotationDir)
     {
         rb.freezeRotation = true;
         transform.Rotate(Vector3.forward * rotationDir * Time.deltaTime);
